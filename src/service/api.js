fc8452830902df1f091aecf858c7dc17c72dbdc9
Kleo Magalhaes <magalhaeskleo@gmail.com>
2021-01-07 14:53:14 -0300

    arrumando a conexão

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://adadcristao-com-br.umbler.net',
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://adadcristao-com-br.umbler.net',
+  //baseURL: 'http://localhost:3333',
   //  baseURL: 'https://rpzbackend.herokuapp.com',
 });
 
